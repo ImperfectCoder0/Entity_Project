c7c8b20d061de146157686dfd56bb833b63a3ee0
ImperfectCoder0 <sriniranjan.gs@gmail.com>
2021-11-24 22:45:04 -0500

    Book Added

--- Classes.py
+++ Classes.py
@@ -1,6 +1,7 @@
 import math
 import base64
 import random
+import json
 
 id_dict = {
     1: "Uncooked Canned Beans",
@@ -14,36 +15,41 @@ id_dict = {
     9: "Pure Light",
     10: "Pure Dark",
     11: "Sorrowed Sword",
-    12: "Sorrowed Digger",
+    12: "Sorrowed Arrow",
     13: "Sorrowed Spear",
     14: "Sorrowed Bow",
     15: "Reaper Sword",
-    16: "Reaper Digger",
+    16: "Reaper Arrow",
     17: "Reaper Spear",
     18: "Reaper Bow",
     19: "Ghost Sword",
-    20: "Ghost Digger",
+    20: "Ghost Arrow",
     21: "Ghost Spear",
     22: "Ghost Bow",
-    23: "Electromagnet Sword",
-    24: "Electromagnet Digger",
-    25: "Electromagnet Spear",
-    26: "Electromagnet Bow",
+    23: "Galaxy Sword",
+    24: "Galaxy Arrow",
+    25: "Galaxy Spear",
+    26: "Galaxy Bow",
     27: "Generation Sword",
-    28: "Generation Digger",
+    28: "Generation Arrow",
     29: "Generation Spear",
     30: "Generation Bow",
     31: "Elder Sword",
-    32: "Elder Digger",
+    32: "Elder Arrow",
     33: "Elder Spear",
     34: "Elder Bow",
     35: "Gladius Deorum",  # Sword of the Gods
-    36: "Fodinarum Somnium",  # Miner's Dream
+    36: "Sagitta Infinitarum Possibilitatum",  # Arrow of Infinite Possibilities
     37: "Hasta Puritatis",  # Spear of Purity
     38: "Custodis Arcus",  # Warden's Bow
-    39: "Wrench",
+    39: "",
+
 
 }
+
+
+# 39 ->
+
 
 
 class Entity:
@@ -63,12 +69,16 @@ b = Entity("i", 20, 40)
 print(b.inventory)
 
 
-class Inventory:
-    def __init__(self, id_, quantity, effects):
+class Inventory_Item:
+    def __init__(self, id_, quantity, effect, durability=None, dmg=1):
         self.id = id_
         self.quantity = quantity
+        self.effect = effect
+        self.dmg = dmg
+
+        if durability is not None: self.durability = durability
+
 
     def use(self, effects):
-        # 3 tools
-        # 7 tiers
+
         pass
