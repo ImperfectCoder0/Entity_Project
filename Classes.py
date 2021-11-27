75a8ae340bb7730c8304e6d4638d122a2c600aa0
ImperfectCoder0 <sriniranjan.gs@gmail.com>
2021-11-27 15:44:29 -0500

    Book.json modified, updated magic armor tiers

--- Classes.py
+++ Classes.py
@@ -5,7 +5,7 @@ import json
 
 id_dict = {
     1: "Uncooked Canned Beans",
-    2: "Cooked Cannced Beans",
+    2: "Cooked Canned Beans",
     3: "Bottled Freshwater",
     4: "Bottled Saltwater",
     5: "Match",
@@ -14,19 +14,19 @@ id_dict = {
     8: "Flashlight 500W",
     9: "Pure Light",
     10: "Pure Dark",
-    11: "Sorrowed Sword",
-    12: "Sorrowed Arrow",
-    13: "Sorrowed Spear",
-    14: "Sorrowed Bow",
-    15: "Reaper Sword",
-    16: "Reaper Arrow",
-    17: "Reaper Spear",
-    18: "Reaper Bow",
-    19: "Ghost Sword",
+    11: "Broken Sword",
+    12: "Broken Arrow",
+    13: "Broken Spear",
+    14: "Broken Bow",
+    15: "Basic Sword",
+    16: "Basic Arrow",
+    17: "Basic Spear",
+    18: "Basic Bow",
+    19: "Ghost Sword",  # Dark
     20: "Ghost Arrow",
     21: "Ghost Spear",
     22: "Ghost Bow",
-    23: "Galaxy Sword",
+    23: "Galaxy Sword",  # Light
     24: "Galaxy Arrow",
     25: "Galaxy Spear",
     26: "Galaxy Bow",
@@ -47,8 +47,6 @@ id_dict = {
 }
 
 
-
-
 class Entity:
     def __init__(self, pos, max_health, resistance):
         self.res = resistance
@@ -58,7 +56,7 @@ class Entity:
         self.accessible = self.inventory[:7]
 
     def attack(self, other, dmg):
-        other.max_health -= dmg - other.resistance
+        other.max_health -= dmg - other.res
         self.accessible[self.selected_item].durability -= 1
 
 
@@ -66,7 +64,7 @@ b = Entity("i", 20, 40)
 print(b.inventory)
 
 
-class Inventory_Item:
+class InventoryItem:
     def __init__(self, id_, quantity, effect, durability=None, dmg=1):
         self.id = id_
         self.quantity = quantity
@@ -78,7 +76,7 @@ class Inventory_Item:
         pass
 
 
-class Upgradable(Inventory_Item):
+class Upgradable(InventoryItem):
     def __init__(self, tier: int, type_: str, id_, quantity, effect, durability, dmg):
         super().__init__(id_, quantity, effect, durability, dmg)
         p_types = ["Sword", "Spear", "Bow", "Arrow",
@@ -86,6 +84,9 @@ class Upgradable(Inventory_Item):
         if type_ in p_types:
             self.tier = tier
             self.type = type_
+
+    def activate_effects(self, owner: Entity):
+        pass
 
 
 class Light(Upgradable):
@@ -98,4 +99,5 @@ class Dark(Upgradable):
 
 class Glitched(Upgradable):
     def corrupt(self): pass
+
     def convert(self): pass
